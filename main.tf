4f7cb0fcb96d0d967fb25a7c466b8fcdf3ddffd5
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-25 23:11:18 +0530

    leaning vault and monitoring ~ auto push

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "tools" {
+  for_each       = var.tools
+  source         = "./module"
+  instance_type  = each.value["instance_type"]
+  port_no        = each.value["port_no"]
+  zone_id        = var.zone_id
+  name           = each.value["name"]
+  policy_actions = each.value["policy_actions"]
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "tools" {
+  default = {
+    vault = {
+      name          = "vault"
+      instance_type = "t3.micro"
+      port_no = {
+        vault = 8200
+      }
+      policy_actions = ["ec2:DescribeAvailabilityZones"]
+    }
+#     prometheus = {
+#       name          = "prometheus"
+#       instance_type = "t3.small"
+#       port_no = {
+#         prometheus = 9090
+#       }
+#       policy_actions = ["ec2:DescribeInstances", "ec2:DescribeAvailabilityZones"]
+#     }
+#     grafana = {
+#       name          = "grafana"
+#       instance_type = "t3.micro"
+#       port_no = {
+#         grafana = 3000
+#       }
+#       policy_actions = ["ec2:DescribeInstances"]
+#     }
+#     elk = {
+#       name          = "elk"
+#       instance_type = "r7a.xlarge"
+#       port_no = {
+#         kibana   = 80
+#         logstash = 5044
+#       }
+#       policy_actions = ["ec2:DescribeInstances"]
+#     }
+  }
+}
+
+variable "zone_id" {
+  default = "Z08061862LBZAM174JIHO"
+}
