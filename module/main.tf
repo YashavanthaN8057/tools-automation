6408411e94678e476afe348014aca033633d16c0
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-26 14:56:14 +0530

    leaning vault and monitoring ~ auto push

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
 
   ami                  = data.aws_ami.main.image_id
   instance_type        = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.main.name
+#   iam_instance_profile = aws_iam_instance_profile.main.name
 
   vpc_security_group_ids = [aws_security_group.main.id]
   instance_market_options {
